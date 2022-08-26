8f979408f0e991c71ee7c193c8e461172704a738
Saiful Islam <md-saiful.islam@stud.uni-bamberg.de>
2022-08-26 18:20:19 +0200

    Problem 1: Find Odd even Description: - odd even is solved

---
+++ OddEven.py
@@ -0,0 +1,6 @@
+userInput = input(" Please enter a number ")
+num = int(userInput)
+if num % 2 ==0 :
+    print(" This is an even numebr")
+else:
+    print("this is an odd number")
---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello world")
+if 5>2:
+    print("5 is grater than 2")
+
+
+x=5
+y="hello worlls"
+print(x)
+print(y)
+
+# fruits=
+

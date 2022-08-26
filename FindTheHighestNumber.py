cb7ef20f090f9e9f30e4dfe23bee02d4c19507a1
Saiful Islam <md-saiful.islam@stud.uni-bamberg.de>
2022-08-26 18:41:52 +0200

    Problem 3: Find the Highest number between 2 number Description: - problem is solved

---
+++ FindTheHighestNumber.py
@@ -0,0 +1,10 @@
+userInput1 =input("Please enter the First Number : ")
+number1= int(userInput1)
+
+userInput2= input("Please enter the Second Number")
+number2 = int(userInput2)
+
+if number1 > number2 :
+    print(number1," is bigger")
+elif number2 > number1 :
+    print(number2, "is bigger")

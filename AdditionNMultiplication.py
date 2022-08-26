d3275e0fb2d0459c86781f44283fc4ca9646c21d
Saiful Islam <md-saiful.islam@stud.uni-bamberg.de>
2022-08-26 21:49:17 +0200

    Problem : Smallest number between 3 number Description: - problem is solved

---
+++ AdditionNMultiplication.py
@@ -0,0 +1,9 @@
+num1= int( input(" Please enter the first number "))
+#print("First number: ", num1)
+num2= int(input("Please enter the second number"))
+#print("Second number: ", num2)
+
+addition = num1 + num2
+multiplication = num1 * num2
+print("Addition = ", addition)
+print(" Multiplication = ", multiplication)
---
+++ CurrencyConversion.py
@@ -0,0 +1,4 @@
+euro = input("Enter the amount in Euro :")
+x=float(euro)
+BDT= x * 94.75
+print("Amount in BDT is :", BDT)
